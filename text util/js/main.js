c9d8ff21804bd4f7ff1706d37c7f3beccfaccd33
kevenallan17 <56761065+kevenallan17@users.noreply.github.com>
2019-11-20 11:05:43 -0300

    Add files via upload

---
+++ text util/js/main.js
@@ -0,0 +1,7 @@
+const texto = document.querySelector('#texto')
+function LowerCase(){
+    texto.value=texto.value.toLowerCase()
+}
+function UpperCase(){
+    texto.value=texto.value.toUpperCase()
+}
